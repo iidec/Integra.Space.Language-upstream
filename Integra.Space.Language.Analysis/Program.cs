e3968e70eafda3a9cdcbb2849a3172433b965ab7
Oscar Roberto Canek Salmerón <oscar.canek@aritec-la.com>
2015-12-21 11:33:27 -0600

    Actualización en la creación del directorio

--- Integra.Space.Language.Analysis/Program.cs
+++ Integra.Space.Language.Analysis/Program.cs
@@ -86,10 +86,6 @@ namespace Integra.Space.Language.Analysis
                 // Try to create the directory.
                 DirectoryInfo di = Directory.CreateDirectory(path);
                 Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
-
-                // Delete the directory.
-                di.Delete();
-                Console.WriteLine("The directory was deleted successfully.");
             }
             catch (Exception e)
             {
