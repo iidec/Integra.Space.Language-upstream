7e962f9839cd5e5d574745a689c6fb00359fe128
Oscar Roberto Canek Salmerón <oscar.canek@aritec-la.com>
2015-12-21 10:51:17 -0600

    Initial Commit for Integra.Space.Language.Analysis

---
+++ Integra.Space.Language.Analysis/Analysis/TreeGraphGenerator.cs
@@ -0,0 +1,125 @@
+﻿using GraphVizWrapper;
+using GraphVizWrapper.Commands;
+using GraphVizWrapper.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integra.Space.Language.Analysis
+{
+    class TreeGraphGenerator
+    {
+        private const string PATH = @"Graphs\";
+        private string fileName;
+        private string outputFile;
+        private int nodeCount;
+
+        public TreeGraphGenerator(string fileName)
+        {
+            this.fileName = fileName;
+            this.outputFile = PATH + fileName + ".png";
+            nodeCount = 0;
+        }
+
+        public void GenerateGraph(PlanNode executionPlan)
+        {
+            var getStartProcessQuery = new GetStartProcessQuery();
+            var getProcessStartInfoQuery = new GetProcessStartInfoQuery();
+            var registerLayoutPluginCommand = new RegisterLayoutPluginCommand(getProcessStartInfoQuery, getStartProcessQuery);
+
+            // GraphGeneration can be injected via the IGraphGeneration interface
+
+            var wrapper = new GraphGeneration(getStartProcessQuery, getProcessStartInfoQuery, registerLayoutPluginCommand);
+
+            string dot = this.ToDotGraph(executionPlan);
+            byte[] output = wrapper.GenerateGraph(dot, Enums.GraphReturnType.Png);
+
+            this.ByteArrayToFile(output);
+        }
+
+        public void ShowGraph()
+        {
+            System.Diagnostics.Process.Start(this.outputFile);
+        }
+
+        private bool ByteArrayToFile(byte[] _ByteArray)
+        {
+            try
+            {
+                // Open file for reading
+                System.IO.FileStream _FileStream = new System.IO.FileStream(this.outputFile, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+                // Writes a block of bytes to this stream using data from
+                // a byte array.
+                _FileStream.Write(_ByteArray, 0, _ByteArray.Length);
+
+                // close file stream
+                _FileStream.Close();
+
+                return true;
+            }
+            catch (Exception _Exception)
+            {
+                // Error
+                Console.WriteLine("Exception caught in process: {0}", _Exception.ToString());
+            }
+
+            // error occured, return false
+            return false;
+        }
+
+        private string ToDotGraph(PlanNode root)
+        {
+            StringBuilder b = new StringBuilder();
+            b.Append("digraph G { node [shape=record]; " + Environment.NewLine);
+            b.Append(ToDot(root, false));
+            this.nodeCount = 0;
+            b.Append(ToDot(root, true));
+            b.Append("}");
+            return b.ToString();
+        }
+
+        private string ToDot(PlanNode node, bool second)
+        {
+            StringBuilder b = new StringBuilder();
+
+            int numRoot = this.nodeCount;
+            if (!second)
+            {
+                b.AppendFormat("{0}[label = \"<f0> {5} |<f1> {1} | {{ Line: {3} | Column: {4} }}\"]; {2}", node.NodeType.ToString() + "_" + numRoot, (node.NodeText == null) ? "" : node.NodeText.Replace("\"", "\\\""), Environment.NewLine, node.Line, node.Column, node.NodeType.ToString());
+            }
+
+            if (node.Children != null)
+            {
+                if (node.Children.Count >= 1)
+                {
+                    int numLeft = ++this.nodeCount;
+                    PlanNode left = node.Children[0];
+                    
+                    if (second)
+                    {
+                        b.AppendFormat("{0}:f0 -> {1}:f0 {2}", node.NodeType.ToString() + "_" + numRoot, left.NodeType.ToString() + "_" + numLeft, Environment.NewLine);
+                    }
+
+                    b.Append(ToDot(left, second));
+                }
+
+                if (node.Children.Count >= 2)
+                {
+                    int numRight = ++this.nodeCount;
+                    PlanNode right = node.Children[1];
+                                        
+                    if (second)
+                    {
+                        b.AppendFormat("{0}:f0 -> {1}:f0 {2}", node.NodeType.ToString() + "_" + numRoot, right.NodeType.ToString() + "_" + numRight, Environment.NewLine);
+                    }
+
+                    b.Append(ToDot(right, second));
+                }
+            }
+
+            return b.ToString();
+        }
+    }
+}
---
+++ Integra.Space.Language.Analysis/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integra.Space.Language.Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string finish = "";
+            while (!finish.ToLower().Equals("stop"))
+            {
+                try
+                {
+
+                    Console.WriteLine("Creating graph directory...");
+                    CreateDirectory();
+                    Console.WriteLine("Write a EQL command or press enter for a test");
+                    Console.WriteLine();
+                    string eql = Console.ReadLine();
+                    Console.WriteLine("Creating execution plan...");
+
+                    if (string.IsNullOrWhiteSpace(eql))
+                    {
+                        eql = string.Format("from {0} where {1} select {2} as CampoNulo",
+                                                                            "SpaceObservable1",
+                                                                            "@event.Message.#0.MessageType == \"0100\"",
+                                                                            "@event.Message.#0.[\"Campo que no existe\"]");
+                    }
+
+                    EQLPublicParser parser = new EQLPublicParser(eql);
+                    List<PlanNode> plan = parser.Parse();
+
+                    Console.WriteLine("Plan generated.");
+                    Console.WriteLine("Creating graph...");
+
+                    string fileName = DateTime.Now.ToString("yyyy_mm_dd hh_mm_ss");
+                    TreeGraphGenerator tgg = new TreeGraphGenerator(fileName);
+                    tgg.GenerateGraph(plan.First());
+
+                    Console.WriteLine("Graph created.");
+                    Console.WriteLine("Opening graph...");
+                    tgg.ShowGraph();
+                    Console.WriteLine("Graph opened.");
+                    Console.WriteLine("Write 'stop' to finish or enter to continue...");
+                    finish = Console.ReadLine();
+                    Console.WriteLine();
+                }
+                catch (Exception e)
+                {
+                    // Get stack trace for the exception with source file information
+                    StackTrace st = new StackTrace(e, true);
+                    // Get the top stack frame
+                    StackFrame frame = st.GetFrame(0);
+                    // Get the line number from the stack frame
+                    int line = frame.GetFileLineNumber();
+                    int column = frame.GetFileColumnNumber();
+
+                    Console.WriteLine("Cannot create the graph. Error: Line: {0}, Column: {1}, Message {2}", line, column, e.Message);
+                    Console.WriteLine("Write 'stop' to finish or enter to continue...");
+                    finish = Console.ReadLine();
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public static void CreateDirectory()
+        {
+            // Specify the directory you want to manipulate.
+            string path = @"Graphs";
+
+            try
+            {
+                // Determine whether the directory exists.
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine("That path exists already.");
+                    return;
+                }
+
+                // Try to create the directory.
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
+
+                // Delete the directory.
+                di.Delete();
+                Console.WriteLine("The directory was deleted successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
+    }
+}
