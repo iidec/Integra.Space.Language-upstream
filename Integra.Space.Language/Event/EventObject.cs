9779b3441111a13fb3898c9b0ee830c7bd878b9f
Oscar Roberto Canek Salmerón <oscar.canek@aritec-la.com>
2016-01-22 11:17:50 -0600

    Traspaso de las clases del Integra.Space.Event a este proyecto

---
+++ Integra.Space.Language/Event/EventObject.cs
@@ -0,0 +1,148 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EventObject.cs" company="Ingetra.Vision.EventObject">
+//     Copyright (c) Ingetra.Vision.EventObject. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Integra.Space.Event
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Messaging;
+
+    /// <summary>
+    /// Event object class
+    /// </summary>
+    [Serializable]
+    public class EventObject
+    {
+        /// <summary>
+        /// Agent 
+        /// Doc go here
+        /// </summary>
+        private EventAgent agent;
+
+        /// <summary>
+        /// adapter
+        /// Doc go here
+        /// </summary>
+        private EventAdapter adapter;
+
+        /// <summary>
+        /// message
+        /// Doc go here
+        /// </summary>
+        private Message message;
+
+        /// <summary>
+        /// List of queries
+        /// </summary>
+        private IEnumerable<string> queries;
+
+        /// <summary>
+        /// Query reference count
+        /// </summary>
+        private int refcount = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventObject"/> class.
+        /// </summary>
+        /// <param name="queries">Queries assigned to the source of events.</param>
+        public EventObject(IEnumerable<string> queries)
+        {
+            this.queries = queries;
+            this.refcount = queries.Count();
+        }
+        
+        /// <summary>
+        /// Gets or sets the agent
+        /// </summary>
+        public EventAgent Agent
+        {
+            get
+            {
+                if (this.agent == null)
+                {
+                    this.agent = new EventAgent();
+                }
+
+                return this.agent;
+            }
+
+            set
+            {
+                this.agent = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the adapter
+        /// </summary>
+        public EventAdapter Adapter
+        {
+            get
+            {
+                if (this.adapter == null)
+                {
+                    this.adapter = new EventAdapter();
+                }
+
+                return this.adapter;
+            }
+
+            set
+            {
+                this.adapter = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the message
+        /// </summary>
+        public Message Message
+        {
+            get
+            {
+                return this.message;
+            }
+
+            set
+            {
+                this.message = value;
+            }
+        }
+
+        /// <summary>
+        /// Increment the reference counter
+        /// </summary>
+        /// <param name="queryName">Query name</param>
+        /// <returns>Indicate if the query name exists en the hash set.</returns>
+        public bool Lock(string queryName)
+        {
+            try
+            {
+                return this.queries.Contains(queryName);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Decrement the reference counter and if is equal to 0 
+        /// </summary>
+        /// <param name="queryName">Query name</param>
+        public void Unlock(string queryName)
+        {
+            if (this.queries.Contains(queryName))
+            {
+                if (System.Threading.Interlocked.Decrement(ref this.refcount) == 0)
+                {
+                    this.message.Dispose();
+                    this.queries = null;
+                }
+            }
+        }
+    }
+}
---
+++ Integra.Space.Language/Query/QueryResult.cs
@@ -0,0 +1,59 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="QueryResult.cs" company="Ingetra.Space.Event">
+//     Copyright (c) Ingetra.Space.Event. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Integra.Space.Query
+{
+    using Integra.Space.Event;
+
+    /// <summary>
+    /// Query result class
+    /// </summary>
+    /// <typeparam name="T">Event result type</typeparam>
+    public class QueryResult<T> where T : EventResult
+    {
+        /// <summary>
+        /// Query identifier
+        /// </summary>
+        private string queryId;
+
+        /// <summary>
+        /// Query result, contains a set of <see cref="EventResult"/> objects
+        /// </summary>
+        private T[] result;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueryResult{T}"/> class.
+        /// </summary>
+        /// <param name="queryId">Query identifier</param>
+        /// <param name="result">Array of event results.</param>
+        public QueryResult(string queryId, T[] result)
+        {
+            this.queryId = queryId;
+            this.result = result;
+        }
+
+        /// <summary>
+        /// Gets the query identifier
+        /// </summary>
+        public string QueryId
+        {
+            get
+            {
+                return this.queryId;
+            }
+        }
+
+        /// <summary>
+        /// Gets the query result
+        /// </summary>
+        public T[] Result
+        {
+            get
+            {
+                return this.result;
+            }
+        }
+    }
+}
