812d72a5eb1cc2aedda29d3b87b5a342947bba3d
Oscar Canek <oscarcanek@gmail.com>
2017-02-03 12:06:05 -0600

    Actualización de la referencia a Integra.Space.Common hacia el proyecto Integra.Space.Common de esta solución

---
+++ Integra.Space.Common/Enums/CommandOptions/SourceOptionEnum.cs
@@ -0,0 +1,38 @@
+﻿﻿//-----------------------------------------------------------------------
+// <copyright file="SourceOptionEnum.cs" company="Integra.Space.Common">
+//     Copyright (c) Integra.Space.Common. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Integra.Space.Common
+{
+    /// <summary>
+    /// Space database option enumerable.
+    /// </summary>
+    internal enum SourceOptionEnum
+    {
+        /// <summary>
+        /// Name option.
+        /// </summary>
+        Name = 1,
+
+        /// <summary>
+        /// Status option.
+        /// </summary>
+        Status = 2,
+
+        /// <summary>
+        /// Cache durability option.
+        /// </summary>
+        Cache_Durability = 3,
+
+        /// <summary>
+        /// Cache size option.
+        /// </summary>
+        Cache_Size = 4,
+
+        /// <summary>
+        /// Persistent option.
+        /// </summary>
+        Persistent = 5
+    }
+}
---
+++ Integra.Space.Common/Enums/CommandOptions/StreamOptionEnum.cs
@@ -0,0 +1,28 @@
+﻿﻿//-----------------------------------------------------------------------
+// <copyright file="StreamOptionEnum.cs" company="Integra.Space.Common">
+//     Copyright (c) Integra.Space.Common. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Integra.Space.Common
+{
+    /// <summary>
+    /// Space database option enumerable.
+    /// </summary>
+    internal enum StreamOptionEnum
+    {
+        /// <summary>
+        /// Name option.
+        /// </summary>
+        Name = 1,
+
+        /// <summary>
+        /// Query option
+        /// </summary>
+        Query = 2,
+
+        /// <summary>
+        /// Status option.
+        /// </summary>
+        Status = 3
+    }
+}
