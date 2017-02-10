b7a6c6a36a05056a6c87a365d02e9bb9b4eda489
Oscar Canek <oscarcanek@gmail.com>
2017-02-10 16:54:24 -0600

    #69 Las clases ResultBase y ResultType fueron creadas

---
+++ Integra.Space.Common/ResultType.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ResultType.cs" company="Integra.Space.Language">
+//     Copyright (c) Integra.Space.Language. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Integra.Space.Common
+{
+    /// <summary>
+    /// Result type enumeration.
+    /// </summary>
+    public enum ResultType
+    {
+        /// <summary>
+        /// Information result.
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// Warning result.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// Error result.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Success result.
+        /// </summary>
+        Success
+    }
+}
