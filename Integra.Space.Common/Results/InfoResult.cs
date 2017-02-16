d9b77fe827fed55a1832af3b5943be417233b658
Oscar Canek <oscarcanek@gmail.com>
2017-02-16 10:52:25 -0600

    Se agregó la documentación a las clases

--- Integra.Space.Common/Results/InfoResult.cs
+++ Integra.Space.Common/Results/InfoResult.cs
@@ -5,8 +5,16 @@
 //-----------------------------------------------------------------------
 namespace Integra.Space.Common
 {
+    /// <summary>
+    /// Infomative result class.
+    /// </summary>
     public class InfoResult : ResultBase
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InfoResult"/> class.
+        /// </summary>
+        /// <param name="code">Result code.</param>
+        /// <param name="message">Result message.</param>
         public InfoResult(int code, string message) : base(code, message, ResultType.Info)
         {
         }
